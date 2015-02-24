03e14266f9d39ac159efb2a0ea2547b4be3a4385
Michal Lacko <viso.laci@gmail.com>
2015-02-24 19:35:35 +0100

    Added url source from google search

---
+++ UrlSource/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open FSharp.Data
+
+module UrlSource =
+
+    let GetGoogleLinks term=
+        HtmlDocument.Load("http://www.google.co.uk/search?q="+term).Descendants ["a"]
+            |> Seq.choose (fun x -> 
+                   x.TryGetAttribute("href")
+                   |> Option.map (fun a -> x.InnerText(), a.Value()))
+            |> Seq.filter (fun (name, url) -> 
+                        name <> "Cached" && name <> "Similar" && url.StartsWith("/url?"))
+            |> Seq.map (fun (_, url) -> url.Substring(0, url.IndexOf("&sa=")).Replace("/url?q=", ""))
+            |> Seq.filter (fun (url) -> url.StartsWith("http"))
+            |> Seq.toArray
+
