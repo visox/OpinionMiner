7df0f0ad6a64f892f1787d765c08acfd705a37c5
Michal Lacko <viso.laci@gmail.com>
2015-02-25 02:04:39 +0100

    added a f# web api

---
+++ OpinionMinerAPI/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Inherits="OpinionMinerAPI.Global" %>
---
+++ OpinionMinerAPI/Models/Car.fs
@@ -0,0 +1,9 @@
+namespace OpinionMinerAPI.Models
+
+open Newtonsoft.Json
+
+[<CLIMutable>]
+type Car = {
+    Make : string
+    Model : string
+}
