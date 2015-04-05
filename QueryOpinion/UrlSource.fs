de192f71c8a9cb3e44b84a13cc072cd7b82d6f2d
michal <viso.laci@gmail.com>
2015-04-05 16:46:51 +0200

    Added bing search from bingApi, for url search.

--- QueryOpinion/UrlSource.fs
+++ QueryOpinion/UrlSource.fs
@@ -2,9 +2,27 @@
 
 open FSharp.Data
 open System
+open System.Net
+open System.Xml
+open System.Data.Services.Client
 
+open BingSearchContainer
 
-let GetGoogleSearchUrl (term, fromDate: Option<DateTime>, toDate: Option<DateTime>) =
+let private bingUrl = "https://api.datamarket.azure.com/Bing/Search"
+let private key = "add from ...https://datamarket.azure.com/..."
+
+let getBingResult query = 
+    let bingContainer = new BingSearchContainer(new Uri(bingUrl)) 
+    bingContainer.Credentials <- new NetworkCredential(key, key)
+    let webQuery = bingContainer.Web(query, null, null, null, null, Nullable<float>(), Nullable<float>(), null)
+    webQuery.Execute() |> ignore
+    [
+        let enumerator = webQuery.GetEnumerator()
+        while enumerator.MoveNext() do
+            yield enumerator.Current.Url 
+    ]
+
+let private GetGoogleSearchUrl (term, fromDate: Option<DateTime>, toDate: Option<DateTime>) =
     let result = "http://www.google.co.uk/search?q=" + term+ "&num=10&lr=lang_en"
     let mutable datePart = 
         match fromDate with
